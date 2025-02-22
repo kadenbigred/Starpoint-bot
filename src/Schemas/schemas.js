50f2ca2e5e2802c8bc6d93a32896bd0f4e749f64
kadenbigred <157423927+kadenbigred@users.noreply.github.com>
2025-02-22 18:42:52 -0500

    fixed typo

--- src/Schemas/schemas.js
+++ src/Schemas/schemas.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Define the schema for the dictionary
 const goodPostsSchema = new mongoose.Schema({
     serverID: {type: String, required: true},
-    userID: {type: String, rwquired: true},
+    userID: {type: String, required: true},
     original: {type: String, required: true},
     board: {type: String, required: true}, 
     reactions: {type: String, required: true},
@@ -11,7 +11,7 @@ const goodPostsSchema = new mongoose.Schema({
 
 const badPostsSchema = new mongoose.Schema({
     serverID: {type: String, required: true},
-    userID: {type: String, rwquired: true},
+    userID: {type: String, required: true},
     original: {type: String, required: true},
     board: {type: String, required: true}, 
     reactions: {type: String, required: true},
