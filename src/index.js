74c570be31de84333589bdfd1ae26742d285fb57
kadenbigred <157423927+kadenbigred@users.noreply.github.com>
2025-04-10 13:04:25 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 token = process.env.TOKEN
 dbtoken = process.env.DB_TOKEN
 
-prefix = '!sx' // You can set this to whatever you want
+prefix = '!sc' // You can set this to whatever you want
 botID = "" // pulled automatically on startup, dont worry about setting this
 settingsDict = {} // set up as key = GuildID, values = {goodChannel, badChannel, goodEmoji, badEmoji, minReacts, adminRole}
 
